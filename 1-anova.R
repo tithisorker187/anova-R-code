ffc3f2fe6225e7ee215d44f370914a8bf2bae5b7
tithi sorker <tsorker46@gmail.com>
2025-03-27 21:07:16 +0600

    Add files via upload

---
+++ 1-anova.R
@@ -0,0 +1,21 @@
+#***************************problem-(1)************************************
+
+##(I)
+
+#input the data
+data=data.frame(
+   Treatment=rep(c("A","B","C","D","E","F"),each=5),
+   Yeild=c(39,26,35,26,24,28,
+           45,43,28,27,28,36,
+           28,32,36,30,31,45,
+           32,40,24,25,29,41,
+           38,29,31,42,44,25)
+)
+data
+anova=aov(Yeild~Treatment,data=data)
+anova
+summary(anova)
+
+
+##(ii)
+confint(anova,level=0.95)
