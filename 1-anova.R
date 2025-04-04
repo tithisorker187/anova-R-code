136b775bfef142fcb395c45fb2297aa46a6d4783
tithi sorker <tsorker46@gmail.com>
2025-04-04 23:49:32 +0600

    Update 1-anova.R

--- 1-anova.R
+++ 1-anova.R
@@ -4,7 +4,7 @@
 
 #input the data
 data=data.frame(
-   Treatment=rep(c("A","B","C","D","E","F"),each=5),
+   Treatment=rep(c("A","B","C","D","E","F"),times=5),
    Yeild=c(39,26,35,26,24,28,
            45,43,28,27,28,36,
            28,32,36,30,31,45,
